f78dc5801e4f6d71a240602b7c82665bb910ea17
Junjie Chen <junjie.chen18@gmail.com>
2019-10-04 09:15:14 -0400

    feat(component): destructure alt prop

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class Image extends Component {
   render() {
-    return <img alt="" />;
+    const { alt } = this.props;
+
+    return <img alt={alt} />;
   }
 }
 
