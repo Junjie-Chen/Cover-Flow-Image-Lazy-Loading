5f26f2a4a89bac1c52c4be20e5b869e62bddc94a
Junjie Chen <junjie.chen18@gmail.com>
2019-10-04 09:25:54 -0400

    feat(component): create observer by calling createObserver instance method

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,6 +3,18 @@ import React, { Component } from 'react';
 class Image extends Component {
   target = React.createRef();
 
+  createObserver = () => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0
+    };
+
+    this.observer = new IntersectionObserver(this.handleIntersect, options);
+
+    this.observer.observe(this.target.current);
+  };
+
   render() {
     const { alt } = this.props;
 
