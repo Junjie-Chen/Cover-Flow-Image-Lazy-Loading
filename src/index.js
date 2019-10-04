9d556f9e3fca0cf77af2a3b71f8e372518834ebf
Junjie Chen <junjie.chen18@gmail.com>
2019-10-04 08:57:33 -0400

    feat(src): connect Apollo Client to React

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import App from './components/App';
 
 const client = new ApolloClient();
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
