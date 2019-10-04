fde7ac7f7c1c7a92ec0a7b1b78907732aeae2116
Junjie Chen <junjie.chen18@gmail.com>
2019-10-04 09:23:38 -0400

    feat(component): add ref prop to store reference to image node

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
 class Image extends Component {
+  target = React.createRef();
+
   render() {
     const { alt } = this.props;
 
-    return <img className="coverflow__cover__25-7e" alt={alt} />;
+    return <img ref={this.target} className="coverflow__cover__25-7e" alt={alt} />;
   }
 }
 
