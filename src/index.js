5b805c16f09626bde44ce4536d0a6b3942a9c9d2
Junjie Chen <junjie.chen18@gmail.com>
2019-10-04 08:56:27 -0400

    feat(src): create Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
 import App from './components/App';
+
+const client = new ApolloClient();
 
 ReactDOM.render(
   <App />,
