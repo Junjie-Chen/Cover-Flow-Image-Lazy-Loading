22fe2426a2991b86c21c6ca3281964e40c90dbbd
Junjie Chen <junjie.chen18@gmail.com>
2019-10-04 08:58:35 -0400

    feat(data): add image data

---
+++ src/data/images.js
@@ -0,0 +1,58 @@
+export default {
+  images: [
+    {
+      id: '1',
+      src: 'https://i.scdn.co/image/a59ba61362a7301facba81a94c9cb1d9d4496b3a',
+      alt: 'Dua Lipa',
+      __typename: 'ImageType'
+    },
+    {
+      id: '2',
+      src: 'https://i.scdn.co/image/d3df5509936cfdaa60b96abd2964bffcb25e80f2',
+      alt: 'MANIA',
+      __typename: 'ImageType'
+    },
+    {
+      id: '3',
+      src: 'https://i.scdn.co/image/52e4a7e4406bba40ff644e0f006044c41448262f',
+      alt: 'Origins',
+      __typename: 'ImageType'
+    },
+    {
+      id: '4',
+      src: 'https://i.scdn.co/image/170e1dee2677f32bdd400c4147eb527d84988d0e',
+      alt: 'Expectations',
+      __typename: 'ImageType'
+    },
+    {
+      id: '5',
+      src: 'https://i.scdn.co/image/93de84650354b3a8436a893332b436cf3bb000d0',
+      alt: 'Shawn Mendes',
+      __typename: 'ImageType'
+    },
+    {
+      id: '6',
+      src: 'https://i.scdn.co/image/38205ab1bdc4b37b68b3ace40989afb6c2c1faa9',
+      alt: 'thank u, next',
+      __typename: 'ImageType'
+    },
+    {
+      id: '7',
+      src: 'https://i.scdn.co/image/df3b17e748de56e4ce78ac29b216d3f99afd0c5a',
+      alt: 'Memories...Do Not Open',
+      __typename: 'ImageType'
+    },
+    {
+      id: '8',
+      src: 'https://i.scdn.co/image/0866257dec39ba72b84382fbab7c49d25faacb0d',
+      alt: 'True Colors',
+      __typename: 'ImageType'
+    },
+    {
+      id: '9',
+      src: 'https://i.scdn.co/image/68c8cb90a5a3fc4ed0413ab6a7e4eac3953aa915',
+      alt: 'KILL THIS LOVE',
+      __typename: 'ImageType'
+    }
+  ]
+};
